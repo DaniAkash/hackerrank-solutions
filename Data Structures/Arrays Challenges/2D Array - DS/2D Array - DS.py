be8f2a1a5c800b7b71032ed15c086560331eba19
DaniAkash <s.daniakash@gmail.com>
2017-04-15 12:42:33 +0530

    2d array - ds

---
+++ Data Structures/Arrays Challenges/2D Array - DS/2D Array - DS.py
@@ -0,0 +1,31 @@
+'''
+Copyright 2017-Present Dani Akash
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import sys
+
+arr = []
+for arr_i in range(6):
+   arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+   arr.append(arr_t)
+  
+max = -548124
+for i in range(6):
+  for j in range(6):
+    if(i+2 < 6 and j+2 < 6):
+      sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+      if sum > max: max = sum
+
+print(max)
